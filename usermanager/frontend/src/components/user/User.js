91e7c18397febb8c504dffa0437e6ee3b2c436fc
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-07-13 20:14:41 +0300

    Connectect redux to users component

--- usermanager/frontend/src/components/user/User.js
+++ usermanager/frontend/src/components/user/User.js
@@ -1,7 +1,20 @@
 import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { getUsers } from "../../actions/users";
 
 class Users extends React.Component {
+  static propTypes = {
+    users: PropTypes.array.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.getUsers();
+  }
+
   render() {
+    console.log("PROPS => ", this.props);
     return (
       <Fragment>
         <div className="outer-container">
@@ -37,4 +50,10 @@ class Users extends React.Component {
     );
   }
 }
-export default Users;
+const mapStateToProps = (state) => {
+  console.log("Main state: ", state);
+  return {
+    users: state.users.users,
+  };
+};
+export default connect(mapStateToProps, { getUsers })(Users);
